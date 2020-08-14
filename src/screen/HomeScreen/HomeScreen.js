1cbe428564946e139bae25ced510edc7bda13ca0
Shubham-Sali <mr.shubhamsali@gmail.com>
2020-08-14 17:31:00 +0530

    Add packages for location and code change for live location tracking

--- src/screen/HomeScreen/HomeScreen.js
+++ src/screen/HomeScreen/HomeScreen.js
@@ -1,20 +1,121 @@
-import React from 'react'
-import { Text, View, Button } from 'react-native'
+import React, { Component } from "react";
+import { StyleSheet, View, AsyncStorage, Text  } from "react-native";
+import MapView from "react-native-maps";
+import * as Location from "expo-location";
+import * as Permissions from "expo-permissions";
+import * as TaskManager from "expo-task-manager";
+import axios from 'axios';
 
-export default function HomeScreen({props,navigation}) {
+const LOCATION_TASK_NAME = "background-location-task";
 
-    const onPressLearnMore = () => {
-        navigation.navigate('Login')
-    }    
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      region: null,
+      error: '',
+    };
+  }
+
+//    const onPressLearnMore = () => {
+//         navigation.navigate('Login')
+//     } 
+
+  _getLocationAsync = async () => {
+    await Location.startLocationUpdatesAsync(LOCATION_TASK_NAME, {
+      enableHighAccuracy: true,
+      distanceInterval: 1,
+      timeInterval: 5000
+    });
+    // watchPositionAsync Return Lat & Long on Position Change
+    this.location = await Location.watchPositionAsync(
+      {
+        enableHighAccuracy: true,
+        distanceInterval: 1,
+        timeInterval: 10000
+      },
+      newLocation => {
+        let { coords } = newLocation;
+        // console.log(coords);
+        let region = {
+          latitude: coords.latitude,
+          longitude: coords.longitude,
+          latitudeDelta: 0.055,
+          longitudeDelta: 0.055
+        };
+        this.setState({ region: region });
+      },
+      error => console.log(error)
+    );
+    return this.location;
+  };
+
+  async componentWillMount() {
+    // Asking for device location permission
+    const { status } = await Permissions.askAsync(Permissions.LOCATION);
+
+    if (status === "granted") {
+      this._getLocationAsync();
+    } else {
+      this.setState({ error: "Locations services needed" });
+    }
+    // userId = (await AsyncStorage.getItem("userId")) || "none";
+    // userName = (await AsyncStorage.getItem("userName")) || "none";
+  }
+
+  render() {
     return (
-        <View>
-            <Text>Home Screen</Text>
-            <Button
+      <View style={styles.container}>
+       <Text style={{ textAlign: 'center', fontWeight: 'bold'}}>Live Location Tracking</Text>
+            {/* <Button
                 onPress={onPressLearnMore}
                 title="Logout"
                 color="#841584"
-            ></Button>
-        </View>
-    )
+            ></Button> */}
+        <MapView
+          initialRegion={this.state.region}
+          showsCompass={true}
+          showsUserLocation={true}
+          rotateEnabled={true}
+          ref={map => {
+            this.map = map;
+          }}
+          style={{ flex: 1 }}
+        />
+      </View>
+    );
+  }
 }
 
+TaskManager.defineTask(LOCATION_TASK_NAME, async ({ data, error }) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  if (data) {
+    const { locations } = data;
+    let lat = locations[0].coords.latitude;
+    let long = locations[0].coords.longitude;
+    // userId = (await AsyncStorage.getItem("userId")) || "none";
+
+    // Storing Received Lat & Long to DB by logged In User Id
+    axios({
+      method: "POST",
+      url: "http://000.000.0.000/phpServer/ajax.php",
+      data: {
+        action: "saveLocation",
+        // userId: userId,
+        lat,
+        long
+      }
+    });
+    console.log("Received new locations for user = ", locations);
+  }
+});
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff"
+  }
+});
